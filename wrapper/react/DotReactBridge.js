010adbd4a068723e341c090c114a9470eb23ca2e
WoncheolHeo <40352090+WoncheolHeo@users.noreply.github.com>
2021-09-08 11:48:25 +0900

    Update DotReactBridge.js

--- wrapper/react/DotReactBridge.js
+++ wrapper/react/DotReactBridge.js
@@ -3,7 +3,7 @@ import {Alert, NativeModules} from 'react-native';
 
 (function(){ 	
 	window.WISETRACKER_SDK_ENV_CODE = 3;
-// 	NativeModules.DotReactBridge.initialization();
 	window.DotReactBridge = NativeModules.DotReactBridge;
-	window.DotReactBridge.initialization();
+	//초기화 호출은 Native에서
+	//window.DotReactBridge.initialization();
 })();
